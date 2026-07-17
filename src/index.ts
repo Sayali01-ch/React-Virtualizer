ba8caf2c1f6b8c00663fa0f4ab8b40f5ebbec3a8
Sanskruti Chopade <sanskruti@example.com>
2026-07-17 14:47:09 +0530

    export statement for VirtualizedInfiniteTable

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// Barrel export so consumers can do: 
+//import VirtualizedInfiniteTable from "./virtualized-infinite-table";
+export{ default } from "./VirtualizedInfinitetable";
+export *from "./types";
