fd141f9034a2e404f337ba0a567391fa010a4646
Sanskruti Chopade <sanskruti@example.com>
2026-07-20 10:39:21 +0530

    fix: correct casing in barrel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // Barrel export so consumers can do: 
 //import VirtualizedInfiniteTable from "./virtualized-infinite-table";
-export{ default } from "./VirtualizedInfinitetable";
+export{ default } from "./VirtualizedInfiniteTable";
 export *from "./types";
