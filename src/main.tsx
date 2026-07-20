601b0b9aa1c0291c764c095bbe58271f46285205
Sanskruti Chopade <sanskruti@example.com>
2026-07-20 13:59:54 +0530

    fix: update import statement

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import VirtualizedInfiniteTable from "./index.tsx";
+import VirtualizedInfiniteTable from "./index";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <VirtualizedInfiniteTable />
